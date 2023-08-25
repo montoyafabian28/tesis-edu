0eeeaba3e254434f947b8e5ac7cc1a61198ef696
Horacio <horaciobriceo2214@gmail.com>
2023-08-25 15:16:27 -0600

    250823

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("daisyui")],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#F6F6F6",
+
+          secondary: "#212121",
+
+          accent: "#205295",
+
+          neutral: "#0A2647",
+
+          "base-100": "#212121",
+
+          "base-200": "#F6F6F6",
+
+          info: "#635985",
+
+          success: "#443C68",
+
+          warning: "#393053",
+
+          error: "#18122B",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
